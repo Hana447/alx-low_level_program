505db44cbcaedad87c343e2dac62cdd82bce023c
Hana447 <alemayehuhana22@gmail.com>
2023-03-16 04:25:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that gives the output using pust function
+ * Return: Return 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
