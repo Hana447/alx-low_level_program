d774299b832f83fa7a59d66e1225c823dbe8a65a
Hana447 <alemayehuhana22@gmail.com>
2023-03-17 18:51:20 -0700

    two combination number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,27 +11,27 @@ int main(void)
 	int q;
 	int s;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10 ; j++)
 	{
-		for (j = 0; j < 10 ; j++)
+		for (q = 0; q < 10; q++)
 		{
-			for (q = 0; q < 10; q++)
+			for (s = 0; s < 10; s++)
 			{
-				for (s = 0; s < 10; s++)
-				{
 				if (i == 0 && j == 0 && q == 0 && s == 0)
 					continue;
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + q);
-					putchar('0' + s);
-					putchar(',');
-					putchar(' ');
-				}
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar(' ');
+				putchar('0' + q);
+				putchar('0' + s);
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+}
 	putchar('\n');
 	return (0);
 }
