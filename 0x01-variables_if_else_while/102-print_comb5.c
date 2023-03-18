e51dc1f2a318986d40512b88e15b3a5e2479be16
Hana447 <alemayehuhana22@gmail.com>
2023-03-17 18:33:49 -0700

    two combination number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+  * main - this is complicated
+  * Return: return 0
+  */
+int main(void)
+{
+
+	int i;
+	int j;
+	int q;
+	int s;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			for (q = 0; q < 10; q++)
+			{
+				for (s = 0; s < 10; s++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + q);
+					putchar('0' + s);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
