a6251a6e6c2c3978b69ea8e0cab09f12b5a54dcd
Hana447 <alemayehuhana22@gmail.com>
2023-03-21 13:42:55 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,25 @@
   */
 void times_table(void)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j < 10)
+		for (j = 0; j < 10; j++)
 		{
-			int n = i * j;
-			if (n < 10)
-			printf("%d,  ", n);
-			printf("%d, ", n);
-			j++;
+			int x = i * j;
+
+			if (j < 9)
+				if (x < 10)
+					printf("%d,  ", x);
+				else
+					printf("%d, ", x);
+			else
+				printf("%d", x);
 		}
 		printf("\n");
-		i++;
 	}
+
 	printf("\n");
 }
