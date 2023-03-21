a5688a6f21ddc363cc0cd3434d5f4ce0edcdebcc
Hana447 <alemayehuhana22@gmail.com>
2023-03-21 06:55:16 -0700

    hour

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * _abs - This function return the absolut value of intiger
+  *
+  * @a :parametr
+  * Return: return 0
+  */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -a;
+	}
+	else
+		a = a;
+	return (a);
+
+}
