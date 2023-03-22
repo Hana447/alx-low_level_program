669fb54d6cf5e3eac4cca35630e88e4f9df66a04
Hana447 <alemayehuhana22@gmail.com>
2023-03-22 01:57:57 -0700

    even number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (i = 3; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5))
-		sum = sum + i
+		if ((i % 3 == 0) || (i % 5 == 0))
+		sum = sum + i;
 	}
 	printf("%d\n", sum);
 	return (0);
