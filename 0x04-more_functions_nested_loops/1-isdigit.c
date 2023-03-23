3784b45280e5583ed1b652558104b99785c3b6f5
Hana447 <alemayehuhana22@gmail.com>
2023-03-23 02:19:06 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *  _isdigit -this function return 1 if c is number or 0 if it is not
+  * Return: return 0
+  * @c:par
+  */
+int _isdigit(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (1);
+}
