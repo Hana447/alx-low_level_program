55dcf1b7227ad143ae1c62641100479c7f0a1eae
Hana447 <alemayehuhana22@gmail.com>
2023-03-23 09:12:06 -0700

    diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,10 +6,9 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 30; j++)
 		{
 			if (j >= 10)
 			{
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,10 +12,14 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+			for (j = 1; j <= n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n'); 
 		}
 	}
 	else
