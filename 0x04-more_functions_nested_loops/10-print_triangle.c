ef8d3d41a1df4adfe8c6195b5a9e1c474e47fcf9
Hana447 <alemayehuhana22@gmail.com>
2023-03-23 10:44:04 -0700

    largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,19 +9,19 @@ void print_triangle(int size)
 	int j;
 	int k;
 	int sizecopy = size;
+
 	if (size > 0)
 	{
 		for (j = 1; j <= size; j++)
-			{
+		{
 				for (k = sizecopy - 1; k > 0; k--)
 					_putchar(' ');
-			
 				for (i = j; i >= 1; i--)
 					_putchar('#');
-	
+
 				_putchar('\n');
 				sizecopy--;
-			}
+		}
 	}
 	else
 		_putchar('\n');
