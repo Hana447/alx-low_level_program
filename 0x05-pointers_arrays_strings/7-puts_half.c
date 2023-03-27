ff7cd3cfbb30b0aa14a4a832df2549bb4a4d4fd6
Hana447 <alemayehuhana22@gmail.com>
2023-03-27 03:02:12 -0700

    Print the second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,12 @@ void puts_half(char *str)
 	int n,i;
 
 	if (len % 2 != 0)
-		n = (len - 1) / 2;
+		n = (len - 2) / 2;
 	else
 		n = len / 2;
 
 	for (i = n; i <= len;i++)
 		putchar(str[i]);
+	putchar('\n');
 }
 
