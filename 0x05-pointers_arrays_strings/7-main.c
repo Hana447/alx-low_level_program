ba7199b40bf91948e0c6b85a423e4bfd962f8e6c
Hana447 <alemayehuhana22@gmail.com>
2023-03-27 02:55:57 -0700

    Print the second half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+	char *new;
+	new = "l01234567896547";
+    str = "0123456789";
+puts_half(new);
+    puts_half(str);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+  * puts_half - This function return the last half numbers
+  * @str: param
+  */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int n,i;
+
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+	else
+		n = len / 2;
+
+	for (i = n; i <= len;i++)
+		putchar(str[i]);
+	putchar('\n');
+}
+
