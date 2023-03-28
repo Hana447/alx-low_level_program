9e6d03760b696a7a95fd98754ed301344bbc371f
Hana447 <alemayehuhana22@gmail.com>
2023-03-28 01:36:53 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,24 +9,32 @@ int _atoi(char *s)
 	 int result = 0;
 	int sign = 1;
 	int found = 0;
+	int min = 0;
 
 	while (*s)
 	{
-		if (*s == '-' && !found)
+		if (*s == '-' && (found == 0 || found == 1))
 		{
-			sign = 1;
+			min += 1;
+			if (min % 2 == 0)
+				sign = 1;
+			else
+				sign = -1;
 			found = 1;
 		}
 		else if (*s == '+' && !found)
 		{
-			sign = -1;
+			sign = 1;
 			found = 1;
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
 			result = result * 10 + (*s - '0');
-			found = 1;
+			found = 2;
 		}
+		else if (!(*s >= '0' && *s <= '9') && found == 2)
+			break;
+		
 		s++;
 
 	}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,7 @@ void puts_half(char *str)
 
 	if (len % 2 != 0)
 	{
-		n = (len - 1) / 2;
-		n++;
+		n = (len + 1) / 2;
 	}
 	else if (len % 2 == 0)
 	{
