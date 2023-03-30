d0c80d25a7590cbae0ab746f4958e90bd45001bd
Hana447 <alemayehuhana22@gmail.com>
2023-03-30 00:18:15 -0700

    Write a function that concatenates two strings. with specified n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
-  * _strcat - This function concatinate 2 string
+  * _strncat - This function concatinate 2 string
   * Return: return character
   * @dest: this is first param
   * @src: this is second param
+  * @n: param 3
   */
 char *_strncat(char *dest, char *src, int n)
 {
