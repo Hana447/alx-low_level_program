71e370c844c84fc4cde4b8f7e5e4dca63a17a869
Hana447 <alemayehuhana22@gmail.com>
2023-04-05 04:34:42 -0700

    A function that return 1 if the number is prime or 0 if not

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,31 @@
 #include "main.h"
 /**
   * _sqrt_recursion - This function return the natural number of a given sqare
-  * _sqrt_helper - This function is suportive function that calculate low
-  * @low: param2
-  * @high: param3
   * @n:param
   * Return: return 0
   */
 int _sqrt_recursion(int n)
 {
-	
 	return (_sqrt_helper(n, 0, n));
 }
+
+/**
+  * _sqrt_helper - this
+  * @n: param1
+  * @low: param2
+  * @high: param3
+  * Return: return 0
+  */
 int _sqrt_helper(int n, int low, int high)
-{	
+{
 	int mid = (low + high) / 2;
 
 	if (low > high)
-		return -1;
+		return (-1);
 	else if (mid * mid == n)
-		return mid;
+		return (mid);
 	else if (mid * mid > n)
-		return _sqrt_helper(n, low, mid-1);
+		return (_sqrt_helper(n, low, mid - 1));
 	else
-		return _sqrt_helper(n, mid+1, high);
+		return (_sqrt_helper(n, mid + 1, high));
 }
