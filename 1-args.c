a1fb360bf74b6bfd9ce1f8a894e3b3ae3af8fb48
Hana447 <alemayehuhana22@gmail.com>
2023-04-07 00:16:25 -0700

    A program that prints the number of arguments, followed by a new line

---
+++ 1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - This is the main function
+  * @argc: parametr1
+  * @argv: parametr2
+  * Return: return0
+  */
+int main(int argc, char *argv[])
+{
+	int i, count = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if(argv[i])
+			count += 1;
+	}
+	printf("%d\n", count);
+	return (0);
+}
