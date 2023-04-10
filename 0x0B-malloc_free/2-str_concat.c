901c7960c05820d245efbdad825a97448867c4f3
Hana447 <alemayehuhana22@gmail.com>
2023-04-10 05:17:39 -0700

    A function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
-  * _strdup - This is function
-  * @str: param1
+  * str_concat - This is function concatinate string
+  * @s1: param1
+  * @s2: param2
   * Return: char
   */
 char *str_concat(char *s1, char *s2)
 {
 	char *t;
-	
+
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	t = malloc(strlen(s1) + strlen(s2) + 1);
 	if (t == NULL)
 		return (NULL);
-	strcpy(t,s1);
-	strcat(t,s2);
+	strcpy(t, s1);
+	strcat(t, s2);
 	return (t);
 }
