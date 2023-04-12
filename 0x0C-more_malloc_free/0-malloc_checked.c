7a7b537d71a273cfc2acdeaddef93a6162426c74
Hana447 <alemayehuhana22@gmail.com>
2023-04-12 13:02:26 -0700

    A function that concatenates two strings.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * malloc_checked -This is function
+  * @b:param1
+  * Return: void
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *t = malloc(b);
+
+	if (t == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit (0);
+	}
+	return (t);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * string_nconcat - This is a function
+  * @s1:param1
+  * @s2:param2
+  * @n:param3
+  * Return: return0
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 =(s1 != NULL) ? strlen(s1) : 0;
+	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
+	char *result = NULL;
+
+	if (n >= len2)
+		n = len2;
+	result = malloc(len1 + n + 1);
+	if (result != NULL)
+	{
+		if (s1 != NULL)
+			memcpy(result, s1, len1);
+		if (s2 != NULL)
+			memcpy(result + len1, s2, n);
+		result[len1 + n] = '\0';
+	}
+	return (result);
+}
