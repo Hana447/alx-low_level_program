07167dde9dd55e3142a107c981d76e895f452ea9
Hana447 <alemayehuhana22@gmail.com>
2023-04-17 07:56:11 -0700

    typedf with struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
-#ifndef  DOG_H
+#ifndef DOG_H
 #define DOG_H
+
+/**
+  * struct dog - This is structure
+  * @name: param1
+  * @age: param2
+  * @owner: param3
+  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } my_dog;
+
 #endif
