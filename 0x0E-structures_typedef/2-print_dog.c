be4a7bfb9e857eb8baf13e5ae05cb66fc2727ea1
Hana447 <alemayehuhana22@gmail.com>
2023-04-17 09:14:56 -0700

    This function assign value to the key

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+  * print_dog - thid is a function
+  * @d:param
+  * Return: return void
+  */
+void print_dog(struct dog *d)
+{
+	if(d == NULL)
+		return;
+	if(d->name != NULL)	
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
