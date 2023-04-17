9aa5709473df7bb2ba1f8d1aac3ac255d45561b0
Hana447 <alemayehuhana22@gmail.com>
2023-04-17 08:58:49 -0700

    This function assign value

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * init_dog - this is a function
+  * @d:param1
+  * @name: param2
+  * @age: param2
+  * @owner: param3
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
