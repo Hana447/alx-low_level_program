68b71a9aa729904b994c5dafa0a60d4162296ab2
Hana447 <alemayehuhana22@gmail.com>
2023-04-21 14:29:27 -0700

    vardic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+/**
+  * sum_them_all - this ia a function
+  * @n: param1
+  * Return: return
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = n;
+
+	sum += ...;
+}
+
