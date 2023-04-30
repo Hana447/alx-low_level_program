889067826a7d2ec4ce0972b58c93747e2c9ce16f
Hana447 <alemayehuhana22@gmail.com>
2023-04-30 10:13:34 -0700

    Free memory 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ * @head: pointer to a pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *now = *head;
+	listint_t *next;
+	
+	while (now)
+	{
+		next = now->next;
+		free(now);
+		now = next;
+	}
+	*head = NULL;
+}
